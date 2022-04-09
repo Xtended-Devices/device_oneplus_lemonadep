61911950884800d9a775c90dd8ef472cac3f71d0
se7ensde <58447233+se7ensde@users.noreply.github.com>
2022-04-09 01:28:24 -0700

    get ready for spark

--- device.mk
+++ device.mk
@@ -25,14 +25,14 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay-spark
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Audio
 PRODUCT_COPY_FILES += \
