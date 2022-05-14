337e0fafd08ca14571ca342d4a32a56f317693f3
se7ensde <se7ensde@gmail.com>
2022-05-14 13:54:23 -0700

    Cherish

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_lemonadep.mk
+    $(LOCAL_DIR)/cherish_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_lemonadep-user \
-    spark_lemonadep-userdebug \
-    spark_lemonadep-eng
+    cherish_lemonadep-user \
+    cherish_lemonadep-userdebug \
+    cherish_lemonadep-eng
